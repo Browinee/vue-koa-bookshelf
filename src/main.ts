eca86a6dddb1eb1c97119dc3157618d11de8a2f5
User <user@AdmindeMacBook-Air.local>
2022-12-09 16:23:22 +0800

    build: add eslint prettier

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
-import { LoadImgs } from "./utils";
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import { LoadImgs } from './utils'
 
-LoadImgs.init();
-console.log(import.meta.env);
-createApp(App).mount("#app");
+LoadImgs.init()
+console.log(import.meta.env)
+createApp(App).mount('#app')
