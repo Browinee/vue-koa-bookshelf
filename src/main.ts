ef370d8158ded19369f2c969ee4690388536e9be
User <user@AdmindeMacBook-Air.local>
2022-12-12 17:49:41 +0800

    refactor: remove files

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import { LoadImgs } from './utils'
-
-LoadImgs.init()
-console.log(import.meta.env)
-createApp(App).mount('#app')
