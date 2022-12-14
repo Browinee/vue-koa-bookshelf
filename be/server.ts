d1ecaad9c3e28f595c90e8000cef1c26eacd3685
User <user@AdmindeMacBook-Air.local>
2022-12-14 10:36:41 +0800

    feat: add server to handle shutdown event

---
+++ be/server.ts
@@ -0,0 +1,25 @@
+import http from "http";
+import app from "./src/app";
+
+const PORT = process.env.PORT || 3003;
+const SERVER = http.createServer(app.callback());
+
+const gracefulShutdown = (msg: any) => {
+  SERVER.close(() => {
+    process.exit();
+  });
+};
+
+SERVER.listen(PORT, () => {
+  // Handle kill commands
+  process.on("SIGTERM", gracefulShutdown);
+
+  // Handle interrupts
+  process.on("SIGINT", gracefulShutdown);
+
+  // Prevent dirty exit on uncaught exceptions:
+  process.on("uncaughtException", gracefulShutdown);
+
+  // Prevent dirty exit on unhandled promise rejection
+  process.on("unhandledRejection", gracefulShutdown);
+});
