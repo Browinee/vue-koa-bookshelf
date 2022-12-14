b8960c2296d372965ca8f9fbff11f54f8d4e1c3a
User <user@AdmindeMacBook-Air.local>
2022-12-14 10:37:55 +0800

    feat: add packages

--- be/src/app.ts
+++ be/src/app.ts
@@ -3,9 +3,24 @@ import Router from "koa-router";
 import body from "koa-body";
 import json from "koa-json";
 import routes from "./routes";
+import { errors, responseTime } from "./middlewares";
+import dotenv from "dotenv";
+import cors from "koa2-cors";
+
+dotenv.config();
 const app = new koa();
 
+app.use(errors);
 app.use(json());
 app.use(body());
 app.use(routes);
-app.listen(3002);
+app.use(
+  cors({
+    origin: "*",
+    allowMethods: ["GET", "POST", "PATCH", "DELETE"],
+    allowHeaders: ["Content-Type", "Accept"],
+    exposeHeaders: ["bookshelf-api-cache", "bookshelf-api-response-time"],
+  })
+);
+app.use(responseTime);
+export default app;
